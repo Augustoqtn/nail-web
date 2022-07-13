faa1dc823cbe94afa2eed55c75df8b8bef3b4048
Augusto <augustinhobrito542@gmail.com>
2022-07-13 16:39:12 -0300

    imprime nome

--- conexao.php
+++ conexao.php
@@ -3,13 +3,17 @@
 $nomeServidor = "127.0.0.1";
 $nomeUsuario = "root";
 $senha = "pw1234";
-$bdNome = "clientes";
+$bdNome = "nails";
 $bdPorta = "3307";
 
 try {
-    $conn = new PDO("mysql:host=$nomeServidor;port=$bdPorta", $nomeUsuario, $senha);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//
+    $conn = new PDO("mysql:host=$nomeServidor;dbname=$bdNome;port=$bdPorta", $nomeUsuario, $senha);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //
 } catch (PDOException $e) {
     echo 'ERROR: ' . $e->getMessage();
+}
+$stmt = $conn->query("SELECT * FROM  clientes");
+while ($row = $stmt->fetch()) {
+    echo $row["nome"];
 }
 
