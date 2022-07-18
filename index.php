83dc39fb99127d586f7e30021d609197d33b134d
Augusto <augustinhobrito542@gmail.com>
2022-07-18 14:29:35 -0300

    tira espacos

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
     <?php
-
     include "conexao.php";
-
     ?>
     <!DOCTYPE html>
     <html lang="en">
@@ -15,22 +13,19 @@
 
     <body>
         <h1>tabela</h1>
-
         <?php $resultadoClientes = $conn->prepare("SELECT * FROM clientes");
         $resultadoClientes->execute(); ?>
-
         <table border=1>
             <tr>
                 <td>nome:</td>
                 <td>telefone:</td>
                 <td>CPF:</td>
             </tr>
-
             <?php while ($linhaCliente = $resultadoClientes->fetch(PDO::FETCH_ASSOC)) : ?>
                 <tr>
                     <td><?php echo $linhaCliente["nome"] ?></td>
                     <td><?php echo $linhaCliente["telefone"] ?></td>
-                    <td><?php echo $linhaCliente["CPF"] ?></td>
+                    <td><?php echo $linhaCliente["cpf"] ?></td>
                 </tr>
             <?php endwhile ?>
         </table>
