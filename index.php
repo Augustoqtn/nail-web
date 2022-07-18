4007f582013d4cf6f8408bc4327c481000791b11
Augusto <augustinhobrito542@gmail.com>
2022-07-18 15:00:02 -0300

    tira espacos3

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
-    <?php
-    include "conexao.php";
-    ?>
-    
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
+<?php
+include "conexao.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Document</title>
+</head>
     <body>
         <h1>tabela</h1>
+        
         <?php $resultadoClientes = $conn->prepare("SELECT * FROM clientes");
         $resultadoClientes->execute(); ?>
         <table border=1>
@@ -20,6 +21,7 @@
                 <td>telefone:</td>
                 <td>CPF:</td>
             </tr>
+
             <?php while ($linhaCliente = $resultadoClientes->fetch(PDO::FETCH_ASSOC)) : ?>
             <tr>
                 <td><?php echo $linhaCliente["nome"] ?></td>
@@ -30,4 +32,4 @@
         </table>
     </body>
 
-    </html>
+</html>
