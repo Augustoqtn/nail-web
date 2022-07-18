e15b110d43f7f08bd4b0f2c1683a2dd31584cd38
Augusto <augustinhobrito542@gmail.com>
2022-07-18 13:41:12 -0300

    melhora a legibilidade do codigo

--- index.php
+++ index.php
@@ -1,51 +1,39 @@
-<?php
+    <?php
 
-include "conexao.php";
+    include "conexao.php";
 
-?>
-<!DOCTYPE html>
-<html lang="en">
+    ?>
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
 
-<body>
-    <h1>tabela</h1>
-    <table border="2">
-        <tr>
-            <td>nome</td>
-            <td>idade</td>
-            <td>CPF</td>
-        </tr>
-        <tr>
-            <td>nome pessoa</td>
-            <td>idade pessoa</td>
-            <td>cpf pessoa</td>
-        </tr>
-    </table>
-</body>
+    <body>
+        <h1>tabela</h1>
 
-</html>
+        <?php $resultadoClientes = $conn->prepare("SELECT * FROM clientes");
+        $resultadoClientes->execute(); ?>
 
-<!-- <?php
-        $resultadoClientes = $conn->prepare("SELECT * FROM clientes");
-        $resultadoClientes->execute();
+        <table border=1>
+            <tr>
+                <td>nome:</td>
+                <td>telefone:</td>
+                <td>CPF:</td>
+            </tr>
 
-        echo "<table border=1>";
-        echo "<tr><td>nome:</td>"
-            . "<td>telefone:</td>"
-            . "<td>CPF</td></tr>";
+            <?php while ($linhaCliente = $resultadoClientes->fetch(PDO::FETCH_ASSOC)) : ?>
+                <tr>
+                    <td><?php echo $linhaCliente["nome"] ?></td>
+                    <td><?php echo $linhaCliente["telefone"] ?></td>
+                    <td><?php echo $linhaCliente["CPF"] ?></td>
+                </tr>
+            <?php endwhile ?>
+        </table>
+    </body>
 
-        while ($linhaCliente = $resultadoClientes->fetch(PDO::FETCH_ASSOC)) {
-
-
-            echo "<tr><td>{$linhaCliente["nome"]}</td>"
-                . "<td>{$linhaCliente["telefone"]}</td>"
-                . "<td>{$linhaCliente["cpf"]}</td></tr>";
-        }
-        echo "</table>";
-        ?> -->
+    </html>
