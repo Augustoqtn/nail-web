c3aa114c431db381325b690bfda3d036b34659e4
Augusto <augustinhobrito542@gmail.com>
2022-07-18 13:21:09 -0300

    tabela fake

--- index.php
+++ index.php
@@ -14,26 +14,38 @@ include "conexao.php";
 </head>
 
 <body>
-    <h1></h1>
-    <?php
-    $resultadoClientes = $conn->prepare("SELECT * FROM clientes");
-    $resultadoClientes->execute();
-
-    echo "<table border=1>";
-    echo "<tr><td>nome:</td>"
-        . "<td>telefone:</td>"
-        . "<td>CPF</td></tr>";
-
-    while ($linhaCliente = $resultadoClientes->fetch(PDO::FETCH_ASSOC)) {
-
-
-        echo "<tr><td>{$linhaCliente["nome"]}</td>"
-            . "<td>{$linhaCliente["telefone"]}</td>"
-            . "<td>{$linhaCliente["cpf"]}</td></tr>";
-    }
-    echo "</table>";
-    ?>
-
+    <h1>tabela</h1>
+    <table border="2">
+        <tr>
+            <td>nome</td>
+            <td>idade</td>
+            <td>CPF</td>
+        </tr>
+        <tr>
+            <td>nome pessoa</td>
+            <td>idade pessoa</td>
+            <td>cpf pessoa</td>
+        </tr>
+    </table>
 </body>
 
 </html>
+
+<!-- <?php
+        $resultadoClientes = $conn->prepare("SELECT * FROM clientes");
+        $resultadoClientes->execute();
+
+        echo "<table border=1>";
+        echo "<tr><td>nome:</td>"
+            . "<td>telefone:</td>"
+            . "<td>CPF</td></tr>";
+
+        while ($linhaCliente = $resultadoClientes->fetch(PDO::FETCH_ASSOC)) {
+
+
+            echo "<tr><td>{$linhaCliente["nome"]}</td>"
+                . "<td>{$linhaCliente["telefone"]}</td>"
+                . "<td>{$linhaCliente["cpf"]}</td></tr>";
+        }
+        echo "</table>";
+        ?> -->
