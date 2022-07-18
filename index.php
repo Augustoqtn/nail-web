417fdf19f0c8d7259934404820c70eb5bbcd74e3
Augusto <augustinhobrito542@gmail.com>
2022-07-18 14:50:35 -0300

    tira espacos2

--- index.php
+++ index.php
@@ -1,16 +1,15 @@
     <?php
     include "conexao.php";
     ?>
+    
     <!DOCTYPE html>
     <html lang="en">
-
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
     </head>
-
     <body>
         <h1>tabela</h1>
         <?php $resultadoClientes = $conn->prepare("SELECT * FROM clientes");
@@ -22,11 +21,11 @@
                 <td>CPF:</td>
             </tr>
             <?php while ($linhaCliente = $resultadoClientes->fetch(PDO::FETCH_ASSOC)) : ?>
-                <tr>
-                    <td><?php echo $linhaCliente["nome"] ?></td>
-                    <td><?php echo $linhaCliente["telefone"] ?></td>
-                    <td><?php echo $linhaCliente["cpf"] ?></td>
-                </tr>
+            <tr>
+                <td><?php echo $linhaCliente["nome"] ?></td>
+                <td><?php echo $linhaCliente["telefone"] ?></td>
+                <td><?php echo $linhaCliente["cpf"] ?></td>
+            </tr>
             <?php endwhile ?>
         </table>
     </body>
