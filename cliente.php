4781a881fe1dacdd4f27e3b13866fe6b75bd537a
Augusto <augustinhobrito542@gmail.com>
2022-07-19 13:59:04 -0300

    lista um cliente por vez

--- cliente.php
+++ cliente.php
@@ -1,5 +1,9 @@
-<!-- <?php
+<?php
+
 include "conexao.php";
+
+$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
+
 ?>
 
 <!DOCTYPE html>
@@ -8,14 +12,27 @@ include "conexao.php";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>cliente</title>
 </head>
 <body>
+    <h2>detalhes do cliente:</h2>
 
-<?php $resultadoClientes = $conn->prepare("SELECT * FROM clientes");
-        $resultadoClientes->execute(); ?>
-        <?php  ?>
-        <?php var_dump($resultadoClientes); ?>
-
+    <?php
+    $resultadoCliente = $conn->prepare("SELECT * FROM clientes WHERE id= :id LIMIT 1");
+    $resultadoCliente->bindParam(":id", $id);
+    $resultadoCliente->execute();
+    $linhaCliente = $resultadoCliente->fetch(PDO::FETCH_ASSOC);?>
+    <table border=1>
+    <tr>
+        <td>Nome</td>
+        <td>telefone</td>
+        <td>Cpf</td>
+    </tr>
+    <tr>
+        <td><?php echo $linhaCliente["nome"]?></td>
+        <td><?php echo $linhaCliente["telefone"]?></td>
+        <td><?php echo $linhaCliente["cpf"]?></td>
+    </tr>
+    </table>
 </body>
-</html> -->
+</html>
--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
+
+
+
 include "conexao.php";
+
 ?>
 
 <!DOCTYPE html>
@@ -8,11 +12,10 @@ include "conexao.php";
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Document</title>
+<title>clientes</title>
 </head>
     <body>
-        <h1>clientes:</h1>
-
+        <h2>clientes:</h2>
         <?php $resultadoClientes = $conn->prepare("SELECT * FROM clientes");
         $resultadoClientes->execute(); ?>
         <table border=1>
