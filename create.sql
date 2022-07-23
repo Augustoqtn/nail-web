c5c417fa7e3b142625f8042695e333788cc394c3
Augusto <augustinhobrito542@gmail.com>
2022-07-23 13:42:50 -0300

    edita cliente

--- create.sql
+++ create.sql
@@ -19,3 +19,7 @@ alter table clientes add column id int(11) not null auto_increment;
 
 -- adiciona o (id) como chave primaria
 add primary key (id);
+
+--edita um cliente 
+update clientes set nome=:nome, telefone=:telefone, cpf=:cpf where id=:id;
+
