94e9e45edb0b6be7ce1d5e50991f0ca8bd0e5fb2
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2022-07-28 16:29:23 -0300

    Refatora formulário de edição

--- editar-cliente.php
+++ editar-cliente.php
@@ -3,70 +3,30 @@ ob_start();
 include "conexao.php";
 $tituloPagina = "Editar cliente";
 include "./templates/cabecalho.php";
+require_once "./src/Clientes/Formulario.php";
 
-$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
+$form = new \Clientes\Formulario($conn, $_GET['id']);
+$form->carregarDoBancoDeDados();
 
-$queryCliente = "SELECT id, nome, telefone, cpf FROM clientes WHERE id = $id LIMIT 1";
-$resultadoCliente = $conn->prepare($queryCliente);
-$resultadoCliente->execute();
 
-if (($resultadoCliente)and($resultadoCliente->rowCount() != 0)) {
-    $linhaCLiente = $resultadoCliente->fetch(PDO::FETCH_ASSOC);
-} else {
-    echo "ERRO! Cliente não encontrado!";
-    exit();
-}   
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $form->definirDados($_POST);
 
-//recebe dados do formulario
-$dadosCliente = filter_input_array(INPUT_POST, FILTER_DEFAULT);
-
-// verifica se usuario clicou no botão
-if (!empty($dadosCliente["editar-cliente"])) {
-    $inputVazio = false;
-    $dadosCliente = array_map("trim",$dadosCliente);
-    if(in_array("", $dadosCliente)) {
-        $inputVazio = true;
+    if ($form->valido()) {
+        $form->salvar();
+        header("Location: /");
+        exit();
+    } else {
         echo "ERRO: Nescessário preencher todos os campos";
-    }
-    if (!$inputVazio) {
-        $queryEditaCliente = "UPDATE clientes SET nome=:nome, telefone=:telefone, cpf=:cpf WHERE id=:id";
-        $editaCliente = $conn->prepare($queryEditaCliente);
-        $editaCliente->bindParam(":nome", $dadosCliente["nome"], PDO::PARAM_STR);
-        $editaCliente->bindParam(":telefone", $dadosCliente["telefone"], PDO::PARAM_STR);
-        $editaCliente->bindParam(":cpf", $dadosCliente["cpf"], PDO::PARAM_STR);
-        $editaCliente->bindParam(":id", $id, PDO::PARAM_INT);
-        if ($editaCliente->execute()) {
-            header("location:index.php");
-            echo "Cliente editado com sucesso!";
-        }
-
     }
 }
 ?>
 <form id="editar-cliente" method="POST" action="">
-    <br><input type="text" id="nome" name="nome" placeholder="Nome completo" value="<?php 
-if (isset($dadosCliente["nome"])) {
-    echo $dadosCliente["nome"];
-} elseif (isset($linhaCLiente["nome"])) {
-    echo $linhaCLiente["nome"];
-    }
-    ?>"><br>
+    <br><input type="text" id="nome" name="nome" placeholder="Nome completo" value="<?php echo $form->getNome() ?>"><br>
 
-    <br><input type="text" id="telefone" name="telefone" placeholder="telefone" value="<?php 
-if (isset($dadosCliente["telefone"])) {
-    echo $dadosCliente["telefone"];
-} elseif (isset($linhaCLiente["telefone"])) {
-    echo $linhaCLiente["telefone"];
-    }
-    ?>"><br>
+    <br><input type="text" id="telefone" name="telefone" placeholder="telefone" value="<?php echo $form->getTelefone() ?>"><br>
     
-    <br><input type="text" id="cpf" name="cpf" placeholder="cpf" value="<?php 
-if (isset($dadosCliente["cpf"])) {
-    echo $dadosCliente["cpf"];
-} elseif (isset($linhaCLiente["cpf"])) {
-    echo $linhaCLiente["cpf"];
-    }
-    ?>"><br>
+    <br><input type="text" id="cpf" name="cpf" placeholder="cpf" value="<?php echo $form->getCpf() ?>"><br>
     <br><input type="submit" value="Salvar" name="editar-cliente"></br>
 </form>
 <?php include "./templates/rodape.php";?>
---
+++ src/Clientes/Formulario.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Clientes;
+
+class Formulario
+{
+    
+    private \PDO $conn;
+    private int $id;
+
+    private array $dados = [
+        'nome' => null,
+        'cpf' => null,
+        'telefone' => null,
+    ];
+
+    public function __construct(\PDO $pdo, int $id = null) 
+    {
+        $this->conn = $pdo;
+        $this->id = $id;
+    }
+
+    public function carregarDoBancoDeDados() 
+    {
+        $sql =  "SELECT id, nome, telefone, cpf FROM clientes WHERE id = :id";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute(['id' => $this->id]);
+        $result = $stmt->fetch();
+        $this->definirDados($result);
+    }
+
+    public function definirDados(array $dados)
+    {
+        $this->dados["nome"] = $dados["nome"];
+        $this->dados["cpf"] = $dados["cpf"];
+        $this->dados["telefone"] = $dados["telefone"];
+    }
+
+    public function getNome() : string
+    {
+        return $this->dados['nome'];
+    }
+
+    public function getCpf() : string
+    {
+        return $this->dados['cpf'];
+    }
+
+    public function getTelefone() : string
+    {
+        return $this->dados['telefone'];
+    }
+
+    public function valido(): bool
+    {
+        $valido = true;
+
+        if (!isset($this->dados["nome"]) || $this->dados["nome"]  === "") {
+            $valido = false;
+        }
+
+        if (!isset($this->dados["telefone"]) || $this->dados["telefone"]  === "") {
+            $valido = false;
+        }
+
+        if (!isset($this->dados["cpf"]) || $this->dados["cpf"]  === "") {
+            $valido = false;
+        }
+
+        return $valido;
+    }
+
+    public function salvar(): void
+    {
+        $params = $this->dados;
+        $params["id"] = $this->id;
+
+        $sql = "UPDATE clientes SET nome = :nome, cpf = :cpf, telefone = :telefone WHERE id = :id";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute($params);
+    }
+}
