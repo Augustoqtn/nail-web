d66e27b506e4f0f423d32f018db683f03ab3939f
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2022-07-28 16:53:52 -0300

    Remove duplicação

--- cadastrar-cliente.php
+++ cadastrar-cliente.php
@@ -5,26 +5,4 @@ require_once "./src/Clientes/Formulario.php";
 
 $form = new \Clientes\Formulario($conn);
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $form->definirDados($_POST);
-
-    if ($form->valido()) {
-        header("Location: /");
-        $form->salvar();
-        exit();
-    } else {
-        echo "ERRO: Nescessário preencher todos os campos";
-    }
-}
-
-include "./templates/cabecalho.php";
-?>
-<form id="editar-cliente" method="POST" action="">
-    <br><input type="text" id="nome" name="nome" placeholder="Nome completo" value="<?php echo $form->getNome() ?>"><br>
-
-    <br><input type="text" id="telefone" name="telefone" placeholder="telefone" value="<?php echo $form->getTelefone() ?>"><br>
-    
-    <br><input type="text" id="cpf" name="cpf" placeholder="cpf" value="<?php echo $form->getCpf() ?>"><br>
-    <br><input type="submit" value="Salvar" name="editar-cliente"></br>
-</form>
-<?php include "./templates/rodape.php";?>
+include "./templates/formulario-cliente.php";
--- editar-cliente.php
+++ editar-cliente.php
@@ -4,29 +4,6 @@ include "conexao.php";
 $tituloPagina = "Editar cliente";
 include "./templates/cabecalho.php";
 require_once "./src/Clientes/Formulario.php";
-
 $form = new \Clientes\Formulario($conn, $_GET['id']);
 $form->carregarDoBancoDeDados();
-
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $form->definirDados($_POST);
-
-    if ($form->valido()) {
-        $form->salvar();
-        header("Location: /");
-        exit();
-    } else {
-        echo "ERRO: Nescessário preencher todos os campos";
-    }
-}
-?>
-<form id="editar-cliente" method="POST" action="">
-    <br><input type="text" id="nome" name="nome" placeholder="Nome completo" value="<?php echo $form->getNome() ?>"><br>
-
-    <br><input type="text" id="telefone" name="telefone" placeholder="telefone" value="<?php echo $form->getTelefone() ?>"><br>
-    
-    <br><input type="text" id="cpf" name="cpf" placeholder="cpf" value="<?php echo $form->getCpf() ?>"><br>
-    <br><input type="submit" value="Salvar" name="editar-cliente"></br>
-</form>
-<?php include "./templates/rodape.php";?>
+include "./templates/formulario-cliente.php";
